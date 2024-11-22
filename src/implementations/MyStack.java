cb53ec43917fcca74b29d2def0d6838a9fcd1f98
TortoiseGhos <152567038+TortoiseGhos@users.noreply.github.com>
2024-11-21 20:26:42 -0700

    fixing all errors
    all junit test errors being fixed and processed

--- src/implementations/MyStack.java
+++ src/implementations/MyStack.java
@@ -49,14 +49,34 @@ public class MyStack<E> implements StackADT<E> {
 
     @Override
     public Object[] toArray() {
-        return elements.toArray();
+        // Create an array with the same size as elements
+        Object[] array = new Object[elements.size()];
+
+        // Loop through the elements in reverse order to match the expected order
+        for (int i = 0; i < elements.size(); i++) {
+            array[i] = elements.get(elements.size() - 1 - i); // Get elements in reverse order
+        }
+
+        return array;
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public E[] toArray(E[] holder) throws NullPointerException {
-        return elements.toArray(holder);
+        // Check if the provided array is too small
+        if (holder.length < elements.size()) {
+            // If the array is too small, create a new array of the correct size
+            holder = (E[]) java.lang.reflect.Array.newInstance(holder.getClass().getComponentType(), elements.size());
+        }
+
+        // Fill the provided array with the elements in the stack (or similar structure)
+        for (int i = 0; i < elements.size(); i++) {
+            holder[i] = elements.get(elements.size() - 1 - i);  // Reverse order
+        }
+
+        return holder;
     }
+
 
     @Override
     public boolean contains(E toFind) throws NullPointerException {
@@ -77,38 +97,40 @@ public class MyStack<E> implements StackADT<E> {
         }
         return -1;
     }
-
     @Override
     public Iterator<E> iterator() {
         return new StackIterator();
     }
 
     private class StackIterator implements Iterator<E> {
-        private int index = 0;
+        private int index; // Start at the top of the stack (last added element)
         private final E[] iteratorElements;
-        
+
         @SuppressWarnings("unchecked")
         public StackIterator() {
-            // Create a copy of elements as required by Iterator interface
+            // Create a copy of elements in reverse order
             iteratorElements = (E[]) new Object[size()];
+            // Populate the array with the elements in LIFO order (from top to bottom)
             for (int i = 0; i < size(); i++) {
-                iteratorElements[i] = elements.get(i);
+                iteratorElements[i] = elements.get(size() - 1 - i); // Reverse the order
             }
+            this.index = 0; // Start the iteration from the top (last element added)
         }
-        
+
         @Override
         public boolean hasNext() {
             return index < iteratorElements.length;
         }
-        
+
         @Override
         public E next() throws NoSuchElementException {
             if (!hasNext()) {
                 throw new NoSuchElementException();
             }
-            return iteratorElements[index++];
+            return iteratorElements[index++]; // Return the next element in LIFO order
         }
     }
+
 
     @Override
     public boolean equals(StackADT<E> that) {
